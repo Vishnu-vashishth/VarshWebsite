13bf3ffd48073766294ff320b8aa11c868550e4a
Vishnu Vashishth <vishnuvashisth31273@gmail.com>
2022-02-26 21:50:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/HeroSection/Hero'
+import Testimonials from './components/Testimonials/Testimonial'
+import Features from './components/Features/Feature'
+import Contact from './components/Contactus/Contact'
+import Footer from './components/Footer/Footer'
+import {Heads} from './components/OurHeads/Heads'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+    <Navbar/>
+    <Hero/>
+    <Features/>
+    <Testimonials/>
+    <Heads/>
+    <Contact/>
+    <Footer/>
+    </>
+  )
 }
 
-export default App;
+export default App
